2b90128bbcd50b1d50412585d2d944357f57ceb7
Tomato <tomatosoupter7678@gmail.com>
2024-11-19 00:36:50 +0000

    Changes

--- src/main/java/net/tomatosoupter/tutorialmod/item/weaponTiers/TutorialModWeaponTiers.java
+++ src/main/java/net/tomatosoupter/tutorialmod/item/weaponTiers/TutorialModWeaponTiers.java
@@ -1,13 +1,24 @@
 package net.tomatosoupter.tutorialmod.item.weaponTiers;
 
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.item.weapons.AttributeContainer;
 import io.redspace.ironsspellbooks.item.weapons.IronsWeaponTier;
+import io.redspace.ironsspellbooks.registries.ItemRegistry;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 
 public class TutorialModWeaponTiers implements Tier, IronsWeaponTier {
+
+    public static TutorialModWeaponTiers VISCERA_SPEAR = new TutorialModWeaponTiers(2   000, 6, -2.8f, 10, BlockTags.INCORRECT_FOR_NETHERITE_TOOL, () -> Ingredient.of(Items.NETHERITE_SCRAP),
+    new AttributeContainer(AttributeRegistry.BLOOD_SPELL_POWER, 0.05, AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+
+
 
     @Override
     public int getUses() {

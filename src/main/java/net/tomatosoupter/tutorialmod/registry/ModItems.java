184f737c6b3b61e7f4a7ec6440f3a4061fb3485c
Ace The King <solcat0414@gmail.com>
2024-11-20 16:00:39 -0500

    Actually fixed spear stats this time - Ace

--- src/main/java/net/tomatosoupter/tutorialmod/registry/ModItems.java
+++ src/main/java/net/tomatosoupter/tutorialmod/registry/ModItems.java
@@ -1,6 +1,12 @@
 package net.tomatosoupter.tutorialmod.registry;
 
+import io.redspace.ironsspellbooks.api.item.weapons.ExtendedSwordItem;
+import io.redspace.ironsspellbooks.api.item.weapons.MagicSwordItem;
+import io.redspace.ironsspellbooks.api.registry.SpellDataRegistryHolder;
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.util.ItemPropertiesHelper;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 import net.neoforged.bus.api.IEventBus;
@@ -24,9 +30,15 @@ public class ModItems {
                     .attributes(SwordItem.createAttributes(Tiers.IRON, 1, -2f))));
 
     //Viscera Spear
-    public static final DeferredItem<SwordItem> VISCERA_SPEAR = ITEMS.register("viscera_spear",
-            () -> new SwordItem(TutorialModWeaponTiers.VISCERA_SPEAR, new Item.Properties()
-                    .attributes(SwordItem.createAttributes(TutorialModWeaponTiers.VISCERA_SPEAR, 0, -2.8f))));
+    public static final DeferredHolder<Item, Item> VISCERA_SPEAR = ITEMS.register("viscera_spear",
+            () -> new SwordItem(TutorialModWeaponTiers.VISCERA_SPEAR, ItemPropertiesHelper.equipment().rarity(Rarity.UNCOMMON).attributes(ExtendedSwordItem.createAttributes(TutorialModWeaponTiers.VISCERA_SPEAR))));
+
+
+    // This is an imbued item, leaving this here for you to reference
+    // Deep Greatsword
+    /*public static final DeferredHolder<Item, Item> DEEP_GREATSWORD = ITEMS.register("deep_greatsword", () ->
+            new MagicSwordItem(DTEWeaponTiers.DEEP_GREATSWORD, ItemPropertiesHelper.equipment().rarity(Rarity.RARE).attributes(ExtendedSwordItem.createAttributes(DTEWeaponTiers.DEEP_GREATSWORD)),
+                    SpellDataRegistryHolder.of(new SpellDataRegistryHolder(SpellRegistry.PLANAR_SIGHT_SPELL, 6))));*/
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
